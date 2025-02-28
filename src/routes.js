c361c26d0f46f696369d35eea263d73897a496f3
512 <flitewka@gmail.com>
2025-02-28 10:54:52 +0100

    Ignore

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
+// @ts-ignore
 import Home from "./routes/Home.svelte";
-import GameDetail from "./routes/GameDetail.svelte";
+// @ts-ignore
 import NotFound from "./routes/NotFound.svelte";
-import Search from "./routes/Search.svelte";
 
 export const routes = {
   "/": Home,
-  "/game/:id": GameDetail,
-  "/search": Search,
   "*": NotFound,
 };
