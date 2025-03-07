47d792b4de8a8000a0890bdc123b2f50613d9981
FilipL <flitewka@gmail.com>
2025-03-07 11:19:29 +0100

    0.1.000

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-// @ts-ignore
-import Home from "./routes/Home.svelte";
-// @ts-ignore
-import NotFound from "./routes/NotFound.svelte";
+import Home from "./routes/Home.svelte"
+import NotFound from "./routes/NotFound.svelte"
+import ExchangeCalculator from "./routes/ExchangeCalculator.svelte"
 
 export const routes = {
   "/": Home,
   "*": NotFound,
+  "/ExchangeCalculator": ExchangeCalculator,
 };
